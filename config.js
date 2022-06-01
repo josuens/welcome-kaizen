e75273a02d4a349403fc82d4154ebb09f277f1b7
Josue Navarro <65885730+josuens@users.noreply.github.com>
2022-06-01 17:43:55 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 /* Make sure to turn on Server Members intent on discord developer portal or you will get some error & won't be able to get welcome/goodbye message & image */
 
 const config = {
-  Token: "",
-  Default_Prefix: "",
-  Color: "",
-  Welcome_Images: "",
-  GoodBye_Images: "",
-  Support: "",
+  Token: "OTgxNzAzNTExMTczNjU2NjI2.GxpnrG.U0HGZZVpQ-LXlSP4mME2itkErJ7_1frwQMmYfY",
+  Default_Prefix: "+",
+  Color: "RED",
+  Welcome_Images: "https://t3.ftcdn.net/jpg/04/53/92/04/360_F_453920491_IrRZc9OZFQeEtMPO41cJ9R8V3vRMeApr.jpg",
+  GoodBye_Images: "https://t3.ftcdn.net/jpg/04/53/92/04/360_F_453920491_IrRZc9OZFQeEtMPO41cJ9R8V3vRMeApr.jpg",
+  Support: "josuenavarro.com",
 };
 
 module.exports = config;
